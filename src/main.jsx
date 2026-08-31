4d58c686e5b55d96bcfe9754ea3a013c178fbb4f
DevRainer <rainergsilva@hotmial.com>
2026-08-31 08:47:11 -0300

    feat: integrate ThemeProvider to enable global theming

--- src/main.jsx
+++ src/main.jsx
@@ -4,33 +4,38 @@ import { RouterProvider } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
+import { ThemeProvider } from 'styled-components';
+
 import { AuthProvider } from './providers/AuthProvider';
 import { CartProvider } from './providers/CartProvider';
 import { UserProvider } from './providers/UserProvider';
 import { router } from './router';
 import GlobalStyles from './styles/globalStyles';
+import { standardTheme } from './styles/themes/standard';
 
 function App() {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <CartProvider>
-          <GlobalStyles />
-          <ToastContainer
-            position="top-right"
-            theme="light"
-            closeButton={false}
-            rtl={false}
-            limit={1}
-            autoClose={1200}
-            hideProgressBar
-            draggable={false}
-            pauseOnHover={false}
-          />
-          <RouterProvider router={router} />
-        </CartProvider>
-      </UserProvider>
-    </AuthProvider>
+    <ThemeProvider theme={standardTheme}>
+      <AuthProvider>
+        <UserProvider>
+          <CartProvider>
+            <GlobalStyles />
+            <ToastContainer
+              position="top-right"
+              theme="light"
+              closeButton={false}
+              rtl={false}
+              limit={1}
+              autoClose={1200}
+              hideProgressBar
+              draggable={false}
+              pauseOnHover={false}
+            />
+            <RouterProvider router={router} />
+          </CartProvider>
+        </UserProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
